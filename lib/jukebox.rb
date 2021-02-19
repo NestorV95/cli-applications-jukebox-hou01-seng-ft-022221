754070788e1c4202365cdeb1d9cdc07efa168a35
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-19 02:16:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,9 +29,8 @@ require"pry"
 
 def play(songs)
  puts "Please enter a song name or number:"
- input = gets.chomp
+ input = gets.strip
  
-  list
   
   songs.each_with_index{|song, index|
    if input == song || input == (index+1).to_s
