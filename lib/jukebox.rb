786d8846b0bf424100b053ee3bbecbf6d399efa8
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-19 02:05:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,13 +30,12 @@ require"pry"
 def play(songs)
  puts "Please enter a song name or number:"
  input = gets.chomp
-  songs.each_with_index do |song, index|
-    
+  songs.each_with_index{|song, index|
    if input == song || input == (index+1).to_s
     puts "Playing #{song}"
   else
     puts "Invalid input, please try again"
-  end 
+  end }
  end
  binding.pry
  
